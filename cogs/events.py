02b417b1258fe1cef51d34768d8d07293446027c
ping-Toven <tomasoliva@outlook.com>
2023-02-08 17:17:42 -0500

    merging

--- cogs/events.py
+++ cogs/events.py
@@ -12,53 +12,7 @@ class Events(commands.Cog):
     def __init__(self, bot:SirenBot):
         self.bot = bot
     
-<<<<<<< HEAD
     """Needs logging channel code, then complete"""
-=======
-    """Incomplete"""
-    @commands.Cog.listener(name='on_guild_role_create')
-    async def scary_role_created(self, role):
-        # Testing, delete when final.
-        channel = role.guild.get_channel(964572977234595910)
-
-        if role.guild.id != 916133196184301668:
-            return
-
-        # Final product.
-        scary_permissions = [role.permissions.administrator, role.permissions.manage_roles, role.permissions.mention_everyone, role.permissions.manage_webhooks, role.permissions.manage_channels, role.permissions.manage_guild]
-
-        if not any(scary_permissions):
-            return
-
-        for permission in role.permissions:
-            await channel.send(permission)
-
-        
-        embed = discord.Embed(title=f'{role} was created with {"x"} dangerous permissions.', color=discord.Color.random())
-        # embed.add_field(name='Permissions:', value='.join(permissions_list)', inline=False)
-
-        # Maybe add a 'who created this role' embed field? - Pattles
-        # You will need to use Audit Log though. - Pattles
-
-
-        # Toven, send this to Jon's channel. - Pattles
-        pass
-
-    """Incomplete"""
-    @commands.Cog.listener(name='on_guild_role_update')
-    async def scary_role_updated(self, before, after):
-        # Testing, delete when final.
-        channel = before.guild.get_channel(964572977234595910)
-
-        if before.guild.id != 916133196184301668:
-            return
-
-        role = before
-
-        # Final product.
-        scary_permissions = [role.permissions.administrator, role.permissions.manage_roles, role.permissions.mention_everyone, role.permissions.manage_webhooks, role.permissions.manage_channels, role.permissions.manage_guild]
-
->>>>>>> main
     @commands.Cog.listener(name='on_webhooks_update')
     async def webhook_updates(self, channel):
         # send_channel exists purely for testing purposes. Replace with a channel from db.
@@ -68,19 +22,11 @@ class Events(commands.Cog):
         # https://discordpy.readthedocs.io/en/stable/api.html#discord.on_webhooks_update
         # https://discordpy.readthedocs.io/en/stable/api.html#discord.Webhook
         
-<<<<<<< HEAD
         """Getting the most recent webhook."""
         webhooks = await channel.webhooks()
         recent_webhook = sorted(webhooks, key=lambda x: x.created_at, reverse=True)[0]
 
         """Sending embed to the logging channel."""
-=======
-        # Getting the most recent webhook.
-        webhooks = await channel.webhooks()
-        recent_webhook = sorted(webhooks, key=lambda x: x.created_at, reverse=True)[0]
-
-        # Sending embed to the logging channel.
->>>>>>> main
         embed = discord.Embed(title='Webhook updated', description=f'A webhook has been updated/created/deleted in {channel.mention}.', color=discord.Color.random(), timestamp=discord.utils.utcnow())
         embed.add_field(name='Webhook Author:', value=
             recent_webhook.user.mention + '\n' + recent_webhook.user.name + '\n' + str(recent_webhook.user.id)
@@ -90,10 +36,9 @@ class Events(commands.Cog):
         embed.set_footer(text='Displayed above is the webhook avatar.')
 
         await send_channel.send(embed=embed)
-
+        
         # await channel.send(embed=embed)
         # Will need to get logging channel info from db, waiting for Toven to create db
-<<<<<<< HEAD
         pass
 
     """Needs logging channel code, then complete"""
@@ -147,7 +92,6 @@ class Events(commands.Cog):
 
 async def setup(bot):
     await bot.add_cog(Events(bot))
-=======
 
 
 
@@ -185,4 +129,3 @@ async def setup(bot):
 
 async def setup(bot):
     await bot.add_cog(Events(bot))
->>>>>>> main
