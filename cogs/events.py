cb4d3715f7714e3ae3eed5130527b7bbe80d4aaa
Pattles <doomrock8@gmail.com>
2023-02-07 22:47:47 -0500

    added on_guild_remove, need log channel db

--- cogs/events.py
+++ cogs/events.py
@@ -55,7 +55,7 @@ class Events(commands.Cog):
         # Final product.
         scary_permissions = [role.permissions.administrator, role.permissions.manage_roles, role.permissions.mention_everyone, role.permissions.manage_webhooks, role.permissions.manage_channels, role.permissions.manage_guild]
 
-    """Partially Complete"""
+    """Needs logging channel code, then complete"""
     @commands.Cog.listener(name='on_webhooks_update')
     async def webhook_updates(self, channel):
         # send_channel exists purely for testing purposes. Replace with a channel from db.
@@ -65,11 +65,11 @@ class Events(commands.Cog):
         # https://discordpy.readthedocs.io/en/stable/api.html#discord.on_webhooks_update
         # https://discordpy.readthedocs.io/en/stable/api.html#discord.Webhook
         
-        # Getting the most recent webhook.
+        """Getting the most recent webhook."""
         webhooks = await channel.webhooks()
         recent_webhook = sorted(webhooks, key=lambda x: x.created_at, reverse=True)[0]
 
-        # Sending embed to the logging channel.
+        """Sending embed to the logging channel."""
         embed = discord.Embed(title='Webhook updated', description=f'A webhook has been updated/created/deleted in {channel.mention}.', color=discord.Color.random(), timestamp=discord.utils.utcnow())
         embed.add_field(name='Webhook Author:', value=
             recent_webhook.user.mention + '\n' + recent_webhook.user.name + '\n' + str(recent_webhook.user.id)
@@ -84,12 +84,13 @@ class Events(commands.Cog):
         # Will need to get logging channel info from db, waiting for Toven to create db
         pass
 
+    """Needs logging channel code, then complete"""
     @commands.Cog.listener(name='on_guild_leave')
     async def bot_leaves_guild(self, guild):
         # send_channel exists purely for testing purposes. Replace with a channel from db.
         send_channel = self.bot.get_channel(964572977234595910)
 
-        # Sending embed to logging channel.
+        """Sending embed to logging channel."""
         embed = discord.Embed(title=f'Bot Left {guild.name}', description=f'{self.bot.user.mention} has just left {guild.name}.', color=discord.Color.random(), timestamp=discord.utils.utcnow())
 
         await send_channel.send(embed=embed)
