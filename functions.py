0ecba1f13fab92ffa91432fb9984a3577cdcc5c9
Pattles <doomrock8@gmail.com>
2023-02-09 15:17:46 -0500

    added get_log_channel, get_gen_chat, get_team_role

--- functions.py
+++ functions.py
@@ -4,8 +4,57 @@ from SirenBot import *
 
 
 def get_guild_id():
+    """
+    Gets guild_id from sirenDB.db
+    rtype: int
+    """
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     cursor.execute(f"SELECT guild_id FROM guild_config;")
     guild_id = cursor.fetchone()[0]
     return guild_id
+
+def get_gen_chat():
+    """
+    Gets the general_channel from sirenDB.db
+    rtype: int
+    """
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT general_channel FROM guild_config;")
+    try:
+        general_channel = cursor.fetchone()[0]
+    except Exception as e:
+        general_channel = None
+        print(e)
+    return general_channel
+
+def get_log_channel():
+    """
+    Gets the log_channel from sirenDB.db
+    rtype: int
+    """
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT log_channel FROM guild_config;")
+    try:
+        log_channel = cursor.fetchone()[0]
+    except Exception as e:
+        log_channel = None
+        print(e)
+    return log_channel
+
+def get_team_role():
+    """
+    Gets the team_role from sirenDB.db
+    rtype: int
+    """
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT team_role FROM guild_config;")
+    try:
+        team_role = cursor.fetchone()[0]
+    except Exception as e:
+        team_role = None
+        print(e)
+    return team_role
