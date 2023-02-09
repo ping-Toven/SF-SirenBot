d29f15c0e913d7029a4ec6747ad0e90b8cad0263
Pattles <doomrock8@gmail.com>
2023-02-09 15:18:47 -0500

    got log channel code from toven, almost complete

--- cogs/events.py
+++ cogs/events.py
@@ -13,11 +13,13 @@ class Events(commands.Cog):
     def __init__(self, bot:SirenBot):
         self.bot = bot
     
-    """Needs logging channel code, then complete"""
+    """Need to figure out how to differentiate between webhook being created, updated, or deleted."""
     @commands.Cog.listener(name='on_webhooks_update')
     async def webhook_updates(self, channel):
-        # send_channel exists purely for testing purposes. Replace with a channel from db.
-        send_channel = self.bot.get_channel(964572977234595910)
+        logging_channel = self.bot.get_channel(get_log_channel()) if get_log_channel() != None else 0
+        
+        if logging_channel == 0:
+            return
 
         # Useful links:
         # https://discordpy.readthedocs.io/en/stable/api.html#discord.on_webhooks_update
@@ -28,7 +30,7 @@ class Events(commands.Cog):
         recent_webhook = sorted(webhooks, key=lambda x: x.created_at, reverse=True)[0]
 
         """Sending embed to the logging channel."""
-        embed = discord.Embed(title='Webhook updated', description=f'A webhook has been updated/created/deleted in {channel.mention}.', color=discord.Color.random(), timestamp=discord.utils.utcnow())
+        embed = discord.Embed(title='Webhook updated', description=f'A webhook has been updated in {channel.mention}.', color=discord.Color.random(), timestamp=discord.utils.utcnow())
         embed.add_field(name='Webhook Author:', value=
             recent_webhook.user.mention + '\n' + recent_webhook.user.name + '\n' + str(recent_webhook.user.id)
             )
@@ -36,7 +38,7 @@ class Events(commands.Cog):
         embed.set_image(url=recent_webhook.display_avatar)
         embed.set_footer(text='Displayed above is the webhook avatar.')
 
-        await send_channel.send(embed=embed)
+        await logging_channel.send(embed=embed)
 
         # await channel.send(embed=embed)
         # Will need to get logging channel info from db, waiting for Toven to create db
