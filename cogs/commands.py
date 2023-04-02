0197d0915b51da1ed1e91c1b2ae10321bcafe76f
Pattles <doomrock8@gmail.com>
2023-04-02 18:33:38 -0400

    added setup cmd

--- cogs/commands.py
+++ cogs/commands.py
@@ -15,9 +15,63 @@ class Commands(commands.Cog):
     def __init__(self, bot:SirenBot):
         self.bot = bot
 
-    @commands.command(description='Say hello to the bot.')
-    async def hello(self, ctx):
-        await ctx.send(f'Hello {ctx.author.mention}!')
+    @commands.command(description='Creates 3 logging webhooks in the channels from `config.env`.')
+    async def setup(self, ctx):     
+        """
+        ## Command is ran in the guild with the logs
+
+        Checks if ctx.author is 'bot_master' from config.env
+            If they aren't, return
+            If they are, continue
+
+        Checks if there are webhooks titled 'mega', 'critical', 'general' anywhere in the server
+            If there is, it deletes them
+
+        Creates 3 webhooks titled 'mega', 'critical', and 'general' in the corresponding channels from config.env
+            If it encounters a perms issue, sends a response & return
+        Stores the webhook URLs in webhook_tokens.json
+        
+        Sends a response
+        """
+
+        """Checks if the user that ran this command is 'bot_master' from config.env"""
+        if ctx.author.id != get_bot_owner():
+            return
+        
+        """Checks if there are webhooks titled 'mega', 'critical', 'general' anywhere in the server"""
+        for webhook in await ctx.guild.webhooks():
+            if webhook.name in ['mega', 'critical', 'SB General Logs']:
+                await webhook.delete()
+                continue
+
+        general_channel = self.bot.get_channel(get_general_logs()) if get_general_logs() != 0 else None
+        critical_channel = self.bot.get_channel(get_critical_logs()) if get_critical_logs() != 0 else None
+        mega_alert_channel = self.bot.get_channel(get_mega_alert_logs()) if get_mega_alert_logs() != 0 else None
+
+        if None in [general_channel, critical_channel, mega_alert_channel]:
+            embed = discord.Embed(title=self.bot.swr, description=self.bot.no_logs, color=self.bot.color)
+            await ctx.send(embed=embed)
+            return
+    
+        """Creates 3 webhooks titled 'mega', 'critical', and 'general' in the corresponding channels from config.env"""
+        try:
+            general_webhook = await general_channel.create_webhook(name='SB General Logs', avatar=await self.bot.user.avatar.read())
+            update_webhook_tokens_json('general', general_webhook.url)
+
+            critical_webhook = await critical_channel.create_webhook(name='SB Critical Logs', avatar=await self.bot.user.avatar.read())
+            update_webhook_tokens_json('critical', critical_webhook.url)
+
+            mega_alert_webhook = await mega_alert_channel.create_webhook(name='SB Mega Alert Logs', avatar=await self.bot.user.avatar.read())
+            update_webhook_tokens_json('mega_alerts', mega_alert_webhook.url)
+        except Exception as e:
+            print(e)
+            embed = discord.Embed(title=self.bot.swr, description='Please ensure you have given me the `Manage Webhooks` permission in the 3 desired channels.', color=self.bot.color)
+            await ctx.send(embed=embed)
+            return
+        
+        """Sending a response"""
+        embed = discord.Embed(description=f'Successfully created 3 logging webhooks in {general_channel.mention}, {critical_channel.mention}, and {mega_alert_channel.mention}.', color=self.bot.color)
+        await ctx.send(embed=embed)
 
     """Need to figure out who's allowed to use this cmd."""
     @commands.hybrid_command(description='Displays the current bot config.')
@@ -92,6 +146,8 @@ class Commands(commands.Cog):
     async def generalchannel(self, ctx, channel:discord.TextChannel):
         # Need code from Toven to add general_channel ID to db.
         await ctx.send(channel.mention)
+
+    
 
 
     
